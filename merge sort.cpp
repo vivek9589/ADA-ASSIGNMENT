c1c23bec60ea288713cf60a153fe8be1cc022e83
vivek9589 <95694472+vivek9589@users.noreply.github.com>
2023-05-15 14:08:00 +0530

    Add files via upload

--- merge sort.cpp
+++ merge sort.cpp
@@ -1,36 +1,74 @@
-// C++ program to merge two sorted arrays
+// Merge sort in C++
+
 #include <iostream>
-#include <bits/stdc++.h>
 using namespace std;
-void merge(int nums1[], int nums2[], int m, int n) {
-    //Creating an array of size m+n to store a merged array
-    int nums3[m+n];
-    // copy nums1[] elements to nums3[]
-    for(int i=0;i<m;i++)
-        nums3[i]=nums1[i];
-    for(int i=0;i<n;i++){
-        int temp=nums2[i];
-        int j=m-1;
-        for(;j>=0;j--){
-            if(nums3[j]>temp){
-                nums3[j+1]=nums3[j];
-            }
-            else
-                break;
-        }
-        m=m+1;
-        nums3[j+1]=temp;
+
+void merge(int arr[], int p, int q, int r) {
+  
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  int L[n1], M[n2];
+
+  for (int i = 0; i < n1; i++)
+    L[i] = arr[p + i];
+  for (int j = 0; j < n2; j++)
+    M[j] = arr[q + 1 + j];
+
+  int i, j, k;
+  i = 0;
+  j = 0;
+  k = p;
+
+  
+  while (i < n1 && j < n2) {
+    if (L[i] <= M[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = M[j];
+      j++;
     }
-    cout << "Array after merging" <<endl;
-    for(int i=0;i<m;i++)
-        cout<<nums3[i]<<" ";
+    k++;
+  }
+
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = M[j];
+    j++;
+    k++;
+  }
 }
-int main(){
-	int nums1[] = {1, 3, 5, 7};
-        int nums2[] = {2, 4, 6, 8};
-	int m = sizeof(nums1) / sizeof(nums1[0]);
-        int n = sizeof(nums2) / sizeof(nums2[0]);
-	// calling our function
-	merge(nums1, nums2, m, n);
-	return 0;
+
+void mergeSort(int arr[], int l, int r) {
+  if (l < r) {
+    int m = l + (r - l) / 2;
+
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+
+    merge(arr, l, m, r);
+  }
+}
+
+void printArray(int arr[], int size) {
+  for (int i = 0; i < size; i++)
+    cout << arr[i] << " ";
+  cout << endl;
+}
+
+int main() {
+  int arr[] = {6, 5, 12, 10, 9, 1};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  mergeSort(arr, 0, size - 1);
+
+  cout << "Sorted array: \n";
+  printArray(arr, size);
+  return 0;
 }
